7bf84826fd8a649f74d84758b892f3a7ef167110
Aliamri Bardi <100109759+aliamribardi@users.noreply.github.com>
2022-04-26 07:41:00 +0800

    Portfolio
    portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        warnaUtama: '#22d3ee',
+        warnaKedua: '#94a3b8',
+        dark: '#0f172a',
+      },
+      screens: {
+        '2xl': '1320px'
+      },
+    },
+  },
+  plugins: [],
+}
